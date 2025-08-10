35cf6ab28f55b00af80adbf0f77a912c5f02ab25
unknown <ehsanullahhaidary@gmail.com>
2025-08-10 13:59:01 +0430

    feat: integration test added

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,26 +1,54 @@
-import { expect, it } from "vitest";
-import { transformToNumber } from "./numbers";
+import { describe, expect, it } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-it("should transform a string number to number", () => {
-  const input = "5";
+describe("transformToNumber()", () => {
+  it("should transform a string number to number", () => {
+    const input = "5";
 
-  const response = transformToNumber(input);
+    const response = transformToNumber(input);
 
-  // first way
-  // const expectedResponse = Number(input);
-  // expect(response).toBe(expectedResponse);
+    // first way
+    // const expectedResponse = Number(input);
+    // expect(response).toBe(expectedResponse);
 
-  // second way
-  expect(response).toBeTypeOf("number");
+    // second way
+    expect(response).toBeTypeOf("number");
+  });
+
+  it("should transform a string number to a number of type number", () => {
+    const input = "1";
+
+    const result = transformToNumber(input);
+
+    expect(result).toBe(+input);
+  });
+
+  it("throws error (NAN) if input is not string or a number", () => {
+    const input = [4, "5"];
+    const input2 = "string";
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 });
 
-it("throws error (NAN) if input is not string or a number", () => {
-  const input = [4, "5"];
-  const input2 = "string";
+describe("cleanNumbers()", () => {
+  it("should return an array of number values if an array of string number values is provided", () => {
+    const numberValues = ["1", "2"];
 
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2);
+    const cleanedNumbers = cleanNumbers(numberValues);
 
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
+
+  it("should thows error for invalid input", () => {
+    const invalidInput = ["", "3"];
+
+    const cleanFn = () => cleanNumbers(invalidInput);
+
+    expect(cleanFn).toThrow();
+  });
 });
